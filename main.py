7335e0f8c4764a20517031632d15251f69572911
VsevolodKozlov-git <vsevolod026@gmail.com>
2020-09-21 17:58:56 +0300

    first stable ver 	modified:   main.py 	aut_data.json

--- main.py
+++ main.py
@@ -1,3 +1,38 @@
-print('hhiello world')
-print('hello, vietnam')
-print('ahahaha')
+from telegram import Update
+from telegram.ext import CallbackContext
+from telegram.ext import Updater
+from telegram.ext import Filters
+from  telegram.ext import  MessageHandler
+import json
+
+
+def load_token():
+    with open('aut_data.json') as j_file:
+        data = json.load(j_file)
+        return data['token']
+
+
+def message_handler(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        text='Sample text'
+    )
+
+def main():
+    """
+        Function that doing all general launch
+        all required functions
+    """
+    # load token
+
+    updater = Updater(token=load_token(), use_context=True)
+    updater.dispatcher.add_handler(MessageHandler(
+        filters=Filters.all,
+        callback=message_handler
+    ))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
